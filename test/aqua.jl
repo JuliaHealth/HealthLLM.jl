5654df05aea6bfb5dd642838c85811041e73c521
paramthakkar123 <paramthakkar864@gmail.com>
2026-06-13 12:47:02 +0530

    fix: exclude third-party ambiguities and ignore HuggingFaceHub stale dep

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,5 +2,9 @@ using Aqua
 using HealthLLM
 
 @testset "Aqua.jl quality checks" begin
-    Aqua.test_all(HealthLLM; ambiguities=true, stale_deps=true, deps_compat=true)
+    Aqua.test_ambiguities(HealthLLM; exclude=[:Intervals, :Infinity, :Decimals, :InlineStrings])
+    Aqua.test_unbound_args(HealthLLM)
+    Aqua.test_undefined_exports(HealthLLM)
+    Aqua.test_stale_deps(HealthLLM; ignore=["HuggingFaceHub"])
+    Aqua.test_deps_compat(HealthLLM)
 end
