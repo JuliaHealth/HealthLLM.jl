cbb034739c4c76a10c17b464cef06df59f3001b6
paramthakkar123 <paramthakkar864@gmail.com>
2026-06-13 13:06:27 +0530

    fix: use Symbol not String for stale_deps ignore

--- test/aqua.jl
+++ test/aqua.jl
@@ -4,6 +4,6 @@ using HealthLLM
 @testset "Aqua.jl quality checks" begin
     Aqua.test_unbound_args(HealthLLM)
     Aqua.test_undefined_exports(HealthLLM)
-    Aqua.test_stale_deps(HealthLLM; ignore=["HuggingFaceHub"])
+    Aqua.test_stale_deps(HealthLLM; ignore=[:HuggingFaceHub])
     Aqua.test_deps_compat(HealthLLM)
 end
