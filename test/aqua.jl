6892abef23119594f547ccaf0faf3fccbb3a73df
paramthakkar123 <paramthakkar864@gmail.com>
2026-06-13 12:35:18 +0530

    fix: remove piracies keyword unsupported by installed Aqua version

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,5 +2,5 @@ using Aqua
 using HealthLLM
 
 @testset "Aqua.jl quality checks" begin
-    Aqua.test_all(HealthLLM; ambiguities=true, stale_deps=true, deps_compat=true, piracies=true)
+    Aqua.test_all(HealthLLM; ambiguities=true, stale_deps=true, deps_compat=true)
 end
