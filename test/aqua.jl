ef10fa0a2668cd08f6cea5d03d8923bdac8dc995
paramthakkar123 <paramthakkar864@gmail.com>
2026-06-13 12:19:40 +0530

    fix: rename Aqua.jl keyword piracy -> piracies

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,5 +2,5 @@ using Aqua
 using HealthLLM
 
 @testset "Aqua.jl quality checks" begin
-    Aqua.test_all(HealthLLM; ambiguities=true, stale_deps=true, deps_compat=true, piracy=true)
+    Aqua.test_all(HealthLLM; ambiguities=true, stale_deps=true, deps_compat=true, piracies=true)
 end
